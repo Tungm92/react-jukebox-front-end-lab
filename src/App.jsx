604c2f61a41a37e255374d00cddfc7f3161abbba
Timothy Weng <timothy880606@gmail.com>
2024-08-08 10:44:23 -0500

    complete trackList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 import TrackList from './components/TrackList/TrackList.jsx';
 import { Route, Routes } from 'react-router-dom';
+import { getTracks, createTrack, updateTrack, deleteTrack } from './services/trackService.js'
+
 
 const App = () => {
   return (
     <>
     <Routes>
-     <Route path="/" element={<TrackList />} />
+      <Route path="/" element={<TrackList getTracks={getTracks}/>} />
     </Routes>
     </>
   );
